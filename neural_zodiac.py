18f112fff079d3cb54cd4345e24437c9d343170f
Jim C. Williams <107096799+jcwml@users.noreply.github.com>
2022-07-04 14:56:55 +0100

    Update neural_zodiac.py

--- neural_zodiac.py
+++ neural_zodiac.py
@@ -41,7 +41,7 @@ aquarius = getFloat()
 print("How many Pisces: ", end='')
 pisces = getFloat()
 
-model = keras.models.load_model("/home/v/Desktop/neural_zodiac/multihot/models/model_tanh_adam_6_32_24_12500_a0.000001/tanh_adam_6_32_24_12500_a0.000001/")
+model = keras.models.load_model("multihot/models/model_tanh_adam_6_32_24_12500_a0.000001/tanh_adam_6_32_24_12500_a0.000001/")
 r = model.predict([[aries, taurus, gemini, cancer, leo, virgo, libra, scorpio, sagittarius, capricorn, aquarius, pisces]], verbose=0)
 print("\nInput Vector: " + istr(aries) + istr(taurus) + istr(gemini) + istr(cancer) + istr(leo) + istr(virgo) + istr(libra) + istr(scorpio) + istr(sagittarius) + istr(capricorn) + istr(aquarius) + istr(pisces))
 print("\nCompatibility: " + "{:.2f}".format(r.flatten()[0]) + '%' + "\n")
